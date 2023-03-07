a6ecd79ff4473660ca9e1fb0837f6433e3cee6ba
Al-Basit <albasitdev@gmail.com>
2023-03-08 02:15:44 +0500

    Weather App

---
+++ js/icons-classes.js
@@ -0,0 +1,54 @@
+
+ const conditions = {
+    0 : 'wi wi-tornado',           // tornado
+    1 : 'wi wi-rain-wind',         // tropical storm
+    2 : 'wi wi-hurricane',         // hurricane
+    3 : 'wi wi-thunderstorm',      // severe thunderstorms
+    4 : 'wi wi-thunderstorm',      // thunderstorms
+    5 : 'wi wi-sleet',             // mixed rain and snow
+    6 : 'wi wi-sleet',             // mixed rain and sleet
+    7 : 'wi wi-sleet',             // mixed snow and sleet
+    8 : 'wi wi-rain-mix',          // freezing drizzle
+    9 : 'wi wi-sprinkle',          // drizzle
+    10 : 'wi wi-snow',             // freezing rain
+    11 : 'wi wi-showers',          // showers
+    12 : 'wi wi-showers',          // showers
+    13 : 'wi wi-snow-wind',        // snow flurries
+    14 : 'wi wi-snow',             // light snow showers
+    15 : 'wi wi-cloudy-gusts',     // blowing snow
+    16 : 'wi wi-snow',             // snow
+    17 : 'wi wi-hail',             // hail
+    18 : 'wi wi-sleet',            // sleet
+    19 : 'wi wi-dust',             // dust
+    20 : 'wi wi-fog',              // foggy
+    21 : 'wi wi-day-haze',         // haze
+    22 : 'wi wi-smoke',            // smoky
+    23 : 'wi wi-strong-wind',      // blustery
+    24 : 'wi wi-windy',            // windy
+    25 : 'wi wi-snowflake-cold',   // cold
+    26 : 'wi wi-cloudy',           // cloudy
+    27 : 'wi wi-night-alt-cloudy', // mostly cloudy (night)
+    28 : 'wi wi-day-cloudy',       // mostly cloudy (day)
+    29 : 'wi wi-night-alt-cloudy', // partly cloudy (night)
+    30 : 'wi wi-day-cloudy',       // partly cloudy (day)
+    31 : 'wi wi-night-clear',      // clear (night)
+    32 : 'wi wi-day-sunny',        // sunny
+    33 : 'wi wi-stars',            // fair (night)
+    34 : 'wi wi-day-sunny',        // fair (day)
+    35 : 'wi wi-hail',             // mixed rain and hail
+    36 : 'wi wi-hot',              // hot
+    37 : 'wi wi-thunderstorm',     // isolated thunderstorms
+    38 : 'wi wi-thunderstorm',     // scattered hunderstorms
+    39 : 'wi wi-thunderstorm',     // scattered thunderstorms
+    40 : 'wi wi-showers',          // scattered showers
+    41 : 'wi wi-snow',             // heavy snow
+    42 : 'wi wi-rain-mix',         // scattered snow showers
+    43 : 'wi wi-snow',             // heavy snow
+    44 : 'wi wi-cloudy',           // partly cloudy
+    45 : 'wi wi-storm-showers',    // thundershowers
+    46 : 'wi wi-snow',             // snow showers
+    47 : 'wi wi-thunderstorm',     // isolated thundershowers
+    3200 : 'wi-alien'              // not available
+};
+
+export {conditions};
---
+++ js/index.js
@@ -0,0 +1,97 @@
+import { conditions } from "./icons-classes.js";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "fc95c8808emsh44f372cf1e795ebp1aaa96jsn2476d4d5c773",
+    "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
+  },
+};
+
+const userInput = document.querySelector("#search-input");
+
+const getWeather = (locationResponse = "islamabad") => {
+  if (!locationResponse) {
+    alert("Please enter a city name.");
+    return;
+  }
+
+  fetch(
+    `https://yahoo-weather5.p.rapidapi.com/weather?location=${locationResponse}&format=json&u=c`,
+    options
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found!");
+      }
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+      currentData(response);
+      upcomingData(response);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+function currentData(response) {
+  document.getElementById("curr-temp").innerHTML =
+    response.current_observation.condition.temperature + "°C";
+  document.getElementById(
+    "curr-location"
+  ).innerHTML = ` ${response.location.city}, ${response.location.country}`;
+  document.getElementById("curr-day&time").innerHTML = new Date(
+    response.current_observation.pubDate * 1000
+  ).toLocaleTimeString("en-US", {
+    timeZone: `${response.location.timezone_id}`,
+    weekday: "long",
+    hour: "numeric",
+    minute: "numeric",
+  });
+  document.getElementById("curr-date").innerHTML = new Date(
+    response.current_observation.pubDate * 1000
+  ).toLocaleDateString("en-US", {
+    timeZone: `${response.location.timezone_id}`,
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  document.getElementById("curr-condition").innerHTML =
+    response.current_observation.condition.text;
+
+  const currCode = response.current_observation.condition.code;
+  const currIcon = conditions[currCode];
+  if (currIcon) {
+    document.getElementById("curr-condition-icon").classList = "";
+    document.getElementById(
+      "curr-condition-icon"
+    ).className = `fs-900 wi ${currIcon.slice(3)}`;
+  }
+}
+
+const days = document.querySelectorAll(".day");
+
+function upcomingData(response) {
+  const forecastData = response.forecasts;
+  days.forEach((dayEl, index) => {
+    const { day, high, low, code } = forecastData[index];
+    dayEl.textContent = day;
+    dayEl.nextElementSibling.nextElementSibling.textContent = ` ${high}°C / ${low}°C`;
+    const iconClass = conditions[code];
+    if (iconClass) {
+      dayEl.nextElementSibling.className = ` wi ${iconClass.slice(3)}`;
+    }
+  });
+}
+
+const searchForm = document.querySelector(".search-form");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = userInput.value.trim();
+  getWeather(location);
+});
+
+getWeather();
